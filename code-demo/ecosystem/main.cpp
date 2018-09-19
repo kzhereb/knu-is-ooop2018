31d574b3b60870a87701e9a6774fd936fdcf4f3e
Kostiantyn Zhereb <zhereb@gmail.com>
2018-09-19 17:03:56 +0300

    Implemented generic population, environment, wolf and rabbit classes
    This code was prepared before lecture 2

--- code-demo/ecosystem/main.cpp
+++ code-demo/ecosystem/main.cpp
@@ -5,8 +5,6 @@
 #include <ctime>
 
 using std::vector;
-using std::cout;
-using std::endl;
 
 struct DayResult {
 	bool grow;
@@ -19,100 +17,327 @@ struct DayResult {
 	}
 };
 
-class Plant {
-private:
-	int age;
-	int waitBreed;
-	const int growAge = 5;
-	const int breedPeriod = 4;
-	const int bornCount = 5;
+template<typename T> class HuntSource {
 public:
-	bool canGrow() {
-		return (age == growAge);
-	}
-	DayResult daily() {
-		DayResult result;
-		age++;
-		if (canGrow()) {
-			result.grow = true;
-			waitBreed = breedPeriod;
-			return result;
-		}
-		if (age < growAge) {
-			return result;
-		}
-
-		if (waitBreed == 0) {
-			result.born = bornCount + rand() % bornCount;
-			waitBreed = breedPeriod;
-		} else {
-			waitBreed--;
-		}
-
-		return result;
-	}
-
-	void print() {
-		cout << "Plant age=" << age << ", wait=" << waitBreed << endl;
-	}
+	virtual T* huntRandom()=0;
 };
 
-class Population {
+template<typename T, typename Prey> class Population: public HuntSource<T> {
 private:
-	vector<Plant*> children;
-	vector<Plant*> adults;
-	int childCount;
-	int adultCount;
+	int child_count;
+	int adult_count;
+	int saturation_count;
+	vector<T*> children;
+	vector<T*> adults;
+	HuntSource<Prey> * preyPopulation;
 public:
-	Population(int starting):  adultCount(0) {
-		for (int i=0;i<starting;i++) {
-			children.push_back(new Plant());
+	Population(int saturation, HuntSource<Prey> * prey = nullptr) {
+		saturation_count = saturation;
+		child_count = 0;
+		adult_count = 0;
+		preyPopulation = prey;
+	}
+	void born(int count) {
+		std::cout << "Born " << count << std::endl;
+		for (int i = 0; i < count; i++) {
+			T* newborn = new T();
+			newborn->print();
+			children.push_back(newborn);
 		}
-		childCount=starting;
+		child_count += count;
+
 	}
 	void daily() {
 		children.erase(
 				std::remove_if(children.begin(), children.end(),
-						[this](Plant* child)->bool {
+						[this](T* child)->bool {
 							DayResult dr = child->daily();
 							if (dr.grow) {
-								childCount--;
-								adultCount++;
 								adults.push_back(child);
+								child_count--;
+								adult_count++;
 								return true;
-							} else {
-								return false;
 							}
+							return false;
 						}), children.end());
+
+		adults.erase(
+				std::remove_if(adults.begin(), adults.end(),
+						[this](T* adult)->bool {
+							if (preyPopulation) {
+								Prey* prey = preyPopulation->huntRandom();
+								adult->hunt(prey);
+							}
+							DayResult dr = adult->daily();
+							if (dr.die) {
+								delete adult;
+								adult_count--;
+								return true;
+							}
+							if (dr.born>0) {
+								born(dr.born);
+							}
+							return false;
+						}), adults.end());
+
+	}
+
+	T* getRandom() {
+		if (adult_count <= 0) {
+			return nullptr;
+		}
+		int rand_idx = rand() % adult_count;
+		T* result = adults[rand_idx];
+		adults.erase(adults.begin() + rand_idx);
+		adult_count--;
+		return result;
+	}
+
+	virtual T* huntRandom() {
+		if (adult_count < saturation_count) {
+			if (rand() % saturation_count > adult_count) {
+				return nullptr;
+			}
+		}
+		return getRandom();
 	}
 
 	void print(bool verbose) {
-		cout<<"childcount="<<childCount<<", adultcount ="<<adultCount<<endl;
+		std::cout << "Population children=" << child_count << ",adults="
+				<< adult_count << std::endl;
 		if (verbose) {
-			cout<<"Children\n";
-			for(Plant* plant:children) {
-				plant->print();
+			std::cout << "Children" << std::endl;
+			for (T* child : children) {
+				child->print();
 			}
-			cout<<"Adults\n";
-			for(Plant* plant:adults) {
-				plant->print();
+			std::cout << "Adults" << std::endl;
+			for (T* adult : adults) {
+				adult->print();
 			}
 		}
 	}
 };
 
+class Plant {
+private:
+	int id;
+	int age;
+	int waitBreed;
+	const int growAge = 5;
+	const int breedPeriod = 4;
+	const int bornCount = 7;
+	static int maxID;
+public:
+	Plant() {
+		age = 0;
+		waitBreed = breedPeriod;
+		maxID++;
+		id = maxID;
+
+	}
+	void hunt(Plant* p) {
+		//plants don't hunt
+	}
+
+	DayResult daily() {
+		DayResult result;
+		age++;
+		if (age == growAge) {
+			result.grow = true;
+			waitBreed = breedPeriod;
+			return result;
+		}
+		if (age > growAge) {
+			waitBreed--;
+			if (waitBreed == 0) {
+				waitBreed = breedPeriod;
+				result.born = bornCount + rand() % bornCount;
+			}
+		}
+		return result;
+	}
+
+	void print() {
+		std::cout << "Plant id=" << id << ",age=" << age << std::endl;
+	}
+};
+int Plant::maxID = 0;
+
+class Rabbit {
+private:
+	int id;
+	int age;
+	int hungry_days;
+	int waitBreed;
+	const int growAge = 4;
+	const int breedPeriod = 3;
+	const int bornCount = 5;
+	static int maxID;
+public:
+	Rabbit() {
+		age = 0;
+		hungry_days = 0;
+		waitBreed = breedPeriod;
+		maxID++;
+		id = maxID;
+
+	}
+	void hunt(Plant* p) {
+
+		if (p) {
+			std::cout << "Rabbit ";
+			this->print();
+			std::cout << " ate plant ";
+			p->print();
+			hungry_days = 0;
+			delete p;
+		} else {
+			hungry_days++;
+		}
+	}
+
+	DayResult daily() {
+		DayResult result;
+		age++;
+		if (age == growAge) {
+			result.grow = true;
+			waitBreed = breedPeriod;
+			return result;
+		}
+		if (age > growAge) {
+			if (hungry_days > 3) {
+				// die of hunger
+				print();
+				std::cout << " died of hunger" << std::endl;
+				result.die = true;
+				return result;
+			}
+			if (waitBreed == 0) {
+				if (hungry_days == 0) {
+					waitBreed = breedPeriod;
+					result.born = bornCount + rand() % bornCount;
+				}
+			} else {
+				waitBreed--;
+			}
+			return result;
+		}
+		return result;
+	}
+
+	void print() {
+		std::cout << "Rabbit id=" << id << ",age=" << age << ", hunger="
+				<< hungry_days << std::endl;
+	}
+};
+int Rabbit::maxID = 0;
+
+class Wolf {
+private:
+	int id;
+	int age;
+	int hungry_days;
+	int waitBreed;
+	const int growAge = 7;
+	const int breedPeriod = 5;
+	const int bornCount = 2;
+	static int maxID;
+public:
+	Wolf() {
+		age = 0;
+		hungry_days = 0;
+		waitBreed = breedPeriod;
+		maxID++;
+		id = maxID;
+
+	}
+	void hunt(Rabbit* p) {
+
+		if (p) {
+			std::cout << "Wolf ";
+			this->print();
+			std::cout << " ate rabbit ";
+			p->print();
+			hungry_days = 0;
+			delete p;
+		} else {
+			hungry_days++;
+		}
+	}
+
+	DayResult daily() {
+		DayResult result;
+		age++;
+		if (age == growAge) {
+			result.grow = true;
+			waitBreed = breedPeriod;
+			return result;
+		}
+		if (age > growAge) {
+			if (hungry_days > 3) {
+				// die of hunger
+				print();
+				std::cout << " died of hunger" << std::endl;
+				result.die = true;
+				return result;
+			}
+			if (waitBreed == 0) {
+				if (hungry_days == 0) {
+					waitBreed = breedPeriod;
+					result.born = bornCount + rand() % bornCount;
+				}
+			} else {
+				waitBreed--;
+			}
+			return result;
+		}
+		return result;
+	}
+
+	void print() {
+		std::cout << "Wolf id=" << id << ",age=" << age << ", hunger="
+				<< hungry_days << std::endl;
+	}
+};
+int Wolf::maxID = 0;
+
+class Environment {
+private:
+	Population<Plant, Plant> plants;
+	Population<Rabbit, Plant> rabbits;
+	Population<Wolf, Rabbit> wolves;
+public:
+	Environment() :
+			plants(30), rabbits(10, &plants), wolves(10,&rabbits) {
+		plants.born(5);
+		rabbits.born(5);
+		wolves.born(2);
+
+	}
+
+	void daily() {
+		plants.daily();
+		rabbits.daily();
+		wolves.daily();
+	}
+	void print(bool verbose) {
+		std::cout << "Plants: ";
+		plants.print(verbose);
+		std::cout << "Rabbits: ";
+		rabbits.print(verbose);
+		std::cout << "Wolves: ";
+		wolves.print(verbose);
+	}
+
+};
+
 int main() {
 	srand(time(NULL));
-//	Plant * plant = new Plant();
-//	for (int i = 0; i < 10; i++) {
-//		plant->daily();
-//		plant->print();
-//	}
-
-	Population pop(5);
-	for (int i = 0; i < 10; i++) {
-			pop.daily();
-			pop.print(true);
-		}
+	Environment env;
+	for (int i = 0; i < 20; i++) {
+		env.daily();
+		std::cout << "===============Day " << i << std::endl;
+		env.print(false);
+	}
 
 }
