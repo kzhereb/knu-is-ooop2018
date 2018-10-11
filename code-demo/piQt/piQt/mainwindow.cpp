e23719c38b1f3f58814211ca0ac6e0a7cdca953e
Kostiantyn Zhereb <zhereb@gmail.com>
2018-10-11 08:11:39 +0300

    Added radio buttons for calculators, implemented clicked slots

--- code-demo/piQt/piQt/mainwindow.cpp
+++ code-demo/piQt/piQt/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "picalculator.h"
+#include <QDebug>
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +14,16 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_rbAtan_clicked()
+{
+    AtanCalculator calc;
+    qDebug()<<"Atan"<<calc.calculate(10);
+}
+
+void MainWindow::on_rbIntegrate_clicked()
+{
+    IntegrateCalculator calc;
+    qDebug()<<"Integrate"<<calc.calculate(10);
 }
