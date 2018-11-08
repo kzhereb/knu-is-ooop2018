4262e5b487d1aca4db27e0215af8586040d43cea
Kostiantyn Zhereb <zhereb@gmail.com>
2018-11-08 13:04:09 +0200

    Check that inherited classes can access protected, but not private
    Also shows that members of base class must be initialized in
    constructors of base class, but can be assigned anywhere

--- code-demo/oopTests/classes.h
+++ code-demo/oopTests/classes.h
@@ -17,6 +17,23 @@ public:
 	int public_field;
 };
 
+class Child: public AccessModifiersSUT {
+public:
+	Child(int pub, int prot)
+		//: public_field(pub), protected_field(prot)
+			// ERROR - must initialize members of base class in base class constructor
+	{
+		public_field = pub;
+			// OK - can assign members of base class from subclass
+		protected_field = prot;
+			// OK - can access protected members of base class from subclass
+		// private_field = 0;
+			// ERROR - subclass cannot access private members of base class
+	}
+	int sum_of_fields() {
+		return public_field + protected_field;
+	}
 
+};
 
 #endif /* CODE_DEMO_OOPTESTS_CLASSES_H_ */
--- code-demo/oopTests/test_classes.cpp
+++ code-demo/oopTests/test_classes.cpp
@@ -12,7 +12,17 @@ TEST_CASE("access modifiers for classes", "[classes]") {
 	AccessModifiersSUT target;
 	target.public_field = 5;
 	REQUIRE(target.public_field == 5);
-	// target.protected_field = 7; ERROR - 'int AccessModifiersSUT::protected_field' is protected within this context
-	// target.private_field = 9;   ERROR - access to private field outside class
+	// target.protected_field = 7;
+		// ERROR - 'int AccessModifiersSUT::protected_field' is protected within this context
+	// target.private_field = 9;
+		// ERROR - access to private field outside class
+
+	SECTION("child class has access to protected") {
+		Child child(5,7);
+		// child.protected_field = 77;
+			// ERROR - can only access protected fields from within class
+		REQUIRE(child.public_field == 5);
+		REQUIRE(child.sum_of_fields() == 12);
+	}
 }
 
