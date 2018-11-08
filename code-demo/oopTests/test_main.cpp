d96b4909a1869de62966354e518501e9fb80139b
Kostiantyn Zhereb <zhereb@gmail.com>
2018-11-08 11:44:01 +0200

    Tutorial on sections

--- code-demo/oopTests/test_main.cpp
+++ code-demo/oopTests/test_main.cpp
@@ -7,6 +7,7 @@
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include <vector>
 
 unsigned int Factorial( unsigned int number ) {
 	return number > 1 ? Factorial(number-1)*number : 1;
@@ -18,6 +19,42 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
     REQUIRE( Factorial(10) == 3628800 );
+}
+
+TEST_CASE( "vectors can be sized and resized", "[vector]" ) {
+
+    std::vector<int> v( 5 );
+
+    SECTION( "move requires to separate section" ) {
+
+		REQUIRE( v.size() == 5 );
+		REQUIRE( v.capacity() >= 5 );
+    }
+
+    SECTION( "resizing bigger changes size and capacity" ) {
+        v.resize( 10 );
+
+        REQUIRE( v.size() == 10 );
+        REQUIRE( v.capacity() >= 10 );
+    }
+    SECTION( "resizing smaller changes size but not capacity" ) {
+        v.resize( 0 );
+
+        REQUIRE( v.size() == 0 );
+        REQUIRE( v.capacity() >= 5 );
+    }
+    SECTION( "reserving bigger changes capacity but not size" ) {
+        v.reserve( 10 );
+
+        REQUIRE( v.size() == 5 );
+        REQUIRE( v.capacity() >= 10 );
+    }
+    SECTION( "reserving smaller does not change size or capacity" ) {
+        v.reserve( 0 );
+
+        REQUIRE( v.size() == 5 );
+        REQUIRE( v.capacity() >= 5 );
+    }
 }
 
 
