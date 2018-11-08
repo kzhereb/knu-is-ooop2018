4a0f0dd69e289d8e5676f2ee4f1f15e6e19d536a
Kostiantyn Zhereb <zhereb@gmail.com>
2018-11-08 12:06:34 +0200

    Testing access modifiers

---
+++ code-demo/oopTests/classes.h
@@ -0,0 +1,22 @@
+/*
+ * classes.h
+ *
+ *  Created on: Nov 8, 2018
+ *      Author: KZ
+ */
+
+#ifndef CODE_DEMO_OOPTESTS_CLASSES_H_
+#define CODE_DEMO_OOPTESTS_CLASSES_H_
+
+class AccessModifiersSUT {
+private:
+	int private_field;
+protected:
+	int protected_field;
+public:
+	int public_field;
+};
+
+
+
+#endif /* CODE_DEMO_OOPTESTS_CLASSES_H_ */
---
+++ code-demo/oopTests/test_classes.cpp
@@ -0,0 +1,18 @@
+/*
+ * test_classes.cpp
+ *
+ *  Created on: Nov 8, 2018
+ *      Author: KZ
+ */
+
+#include "classes.h"
+#include "catch.hpp"
+
+TEST_CASE("access modifiers for classes", "[classes]") {
+	AccessModifiersSUT target;
+	target.public_field = 5;
+	REQUIRE(target.public_field == 5);
+	// target.protected_field = 7; ERROR - 'int AccessModifiersSUT::protected_field' is protected within this context
+	// target.private_field = 9;   ERROR - access to private field outside class
+}
+
