127cec9567308c95f098930230af86d4e9d3b38c
Kostiantyn Zhereb <zhereb@gmail.com>
2019-03-25 12:22:24 +0200

    Initial version of matrix factory

--- code-demo/patterns/MatrixQt/main.cpp
+++ code-demo/patterns/MatrixQt/main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "matrix.h"
+#include "matrixfactory.h"
 #include <QApplication>
 #include <QDebug>
 
 int main(int argc, char *argv[])
 {
-    Matrix<int> * matr = new Matrix1D<int>(4,5);
+    Matrix1DFactory<int> factory;
+    Matrix<int> * matr = factory.createMatrix(4,5);
     matr->set(1,3,12);
     qDebug()<< matr->get(1,3);
 
--- code-demo/patterns/MatrixQt/matrixfactory.h
+++ code-demo/patterns/MatrixQt/matrixfactory.h
@@ -1,11 +1,36 @@
 #ifndef MATRIXFACTORY_H
 #define MATRIXFACTORY_H
 
+#include "matrix.h"
 
+template <typename T>
 class MatrixFactory
 {
 public:
-    MatrixFactory();
+    MatrixFactory()=default;
+    virtual Matrix<T>* createMatrix(int rows, int columns)=0;
 };
+
+
+template <typename T>
+class Matrix2DFactory: public MatrixFactory<T>
+{
+public:
+    Matrix2DFactory():MatrixFactory<T>() {}
+    Matrix<T>* createMatrix(int rows, int columns) {
+        return new Matrix2D<T>(rows, columns);
+    }
+};
+
+template <typename T>
+class Matrix1DFactory: public MatrixFactory<T>
+{
+public:
+    Matrix1DFactory():MatrixFactory<T>() {}
+    Matrix<T>* createMatrix(int rows, int columns) {
+        return new Matrix1D<T>(rows, columns);
+    }
+};
+
 
 #endif // MATRIXFACTORY_H
